112df795e2f4ab86538b1899291b281c66e2cee4
Fadhoo <fadipetomi00@gmail.com>
2020-08-22 19:47:28 -0700

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\FADO\Documents\django projects\inventory-management\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
